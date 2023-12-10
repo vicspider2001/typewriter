018392262d74497bb2369baa7e85bfd8735c83e9
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-10 15:56:34 -0500

    Assignment

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
